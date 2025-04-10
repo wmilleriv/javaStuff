02e508d77050490dcdf13f86a1f429677a22408a
William Miller <Wmilleiv@gmail.com>
2025-04-10 14:31:07 -0400

    reset

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for(int i=0; i<=10;i++){
-            for(int k=0;k<i;k++){
-                System.out.print(k);
-            }
-            System.out.println();
         }
-        for(int i=10; i>=0;i--){
-            for(int k=0;k<i;k++){
-                System.out.print(k);
-            }
-            System.out.println();
-        }}
     }
 }
