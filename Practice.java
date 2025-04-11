3935d0f9cc861d83a0484d73dbd4d3dccdf18b8e
William Miller <Wmilleiv@gmail.com>
2025-04-11 12:01:39 -0400

    scanner

--- Practice.java
+++ Practice.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class Practice{
     public static void main(String[] args){
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
     }
 }
