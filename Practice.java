cfd4e800fd1006a3ea772bdcf66b6b7c2f0b5b1c
William Miller <Wmilleiv@gmail.com>
2025-04-15 12:43:10 -0400

    Java scanner

--- Practice.java
+++ Practice.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 public class Practice{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = sc.nextDouble();
+        boolean c = sc.nextBoolean();
+
 
         sc.close();
     }
